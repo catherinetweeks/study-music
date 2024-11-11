0e22f3df013073a66158f28c7f69eca0c19fbb52
Catherine Weeks <catherinetweeks@gmail.com>
2024-11-10 20:54:29 -0500

    html boilerplate + started working on the lists of playlists

---
+++ app.js
@@ -0,0 +1,22 @@
+// create lists for each 'genre'
+const soothingList = [
+    'https://www.youtube.com/watch?v=rqRp0_O4ZaU',
+    'https://www.youtube.com/watch?v=xFvwf199hPE&list=PL1bLldq1EpZb166P2KBlucQJDL_WzgUOi&index=33&t=13055s',
+    
+
+]
+
+const upbeatList = [
+    'https://www.youtube.com/watch?v=inYhcA3ZhzQ',
+    'https://www.youtube.com/watch?v=8T9rQQwfYgQ&list=PL1bLldq1EpZb166P2KBlucQJDL_WzgUOi&index=19&t=846s',
+
+
+]
+
+const classicalList = [
+    'https://www.youtube.com/watch?v=7e7eaAD7tus',
+    'https://www.youtube.com/watch?v=YYeHdpdjoeg',
+    'https://www.youtube.com/watch?v=CyqHICzufP8&t=1s',
+    'https://www.youtube.com/watch?v=pXD2rZHzIns&t=550s'
+  ];
+
